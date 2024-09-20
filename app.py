2a12892e2acac64885995d109121116bcd3a95cc
hapijul-trigent <hapijul_h@trigent.com>
2024-09-20 09:21:10 +0530

    working good update

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 from PIL import Image
 from src.tryon import InpaintingPipeline, load_and_process_image
-from src.body_segmentation import segment_body
+from SegBody import segment_body
 import torch
 from diffusers import AutoPipelineForInpainting, AutoencoderKL
 from diffusers.utils import load_image
@@ -15,7 +15,20 @@ st.set_page_config(
     layout="wide",
     initial_sidebar_state="collapsed",
 )
-
+# Add logo and title
+logo_path = "https://trigent.com/wp-content/uploads/Trigent_Axlr8_Labs.png"
+st.markdown(
+    f"""
+    <div style="text-align: center;">
+        <img src="{logo_path}" alt="Trigent Logo" style="max-width:100%;">
+    </div>
+    """,
+    unsafe_allow_html=True
+)
+# Main Page Title and Caption
+st.title("Trigent - Virtual Try-On")
+st.caption("Streamlit application that leverages a pre-trained model from Hugging Face to provide a virtual clothing try-on feature.")
+st.divider()
 
 def get_or_create_session_state_variable(key, default_value=None):
     """
@@ -72,7 +85,6 @@ def virtual_try_on(pipeline, img, clothing, prompt, negative_prompt, ip_scale=1.
 
 vton_pipeline = virtual_try_on_pipeline()
 
-st.title("Virtual Clothing Try-On")
 col1, col2, col3 = st.columns(3)
 
 with col1:
@@ -95,18 +107,57 @@ settings = {
     "steps": st.slider("Inference Steps", min_value=10, max_value=200, value=100)
 }
 
-if st.button("Generate"):
-    print("Ready to generate...")
-    if person_image_file and cloth_image_file:
-        person_img = load_image('person.jpg').convert("RGB")
-        cloth_img = load_image('cloth.jpg').convert("RGB")
-        # final_image = app.generate_image(person_img, cloth_img, body_mask_image=body_mask_image, settings=settings)
-        generated_image = virtual_try_on(
-            pipeline, img=person_img, clothing=cloth_img, 
-            prompt=settings['prompt'], negative_prompt=settings['negative_prompt'], 
-            ip_scale=1.0, strength=0.99, guidance_scale=7.5, steps=100
-        )
-        with col3:
-          st.image(generated_image, caption="Person Wearing Clothing", use_column_width=True)
-    else:
-        st.warning("Please upload both person and clothing images.")
+with col3:
+  st.header('')
+
+  if st.button("Try On Cloth", use_container_width=True):
+      print("Ready to generate...")
+      if person_image_file and cloth_image_file:
+          person_img = load_image('person.jpg').convert("RGB")
+          cloth_img = load_image('cloth.jpg').convert("RGB")
+          # final_image = app.generate_image(person_img, cloth_img, body_mask_image=body_mask_image, settings=settings)
+          generated_image = virtual_try_on(
+              vton_pipeline, img=person_img, clothing=cloth_img, 
+              prompt=settings['prompt'], negative_prompt=settings['negative_prompt'], 
+              ip_scale=1.0, strength=settings['strength'], guidance_scale=settings['guidance_scale'], steps=settings['steps']
+          )
+          with col3:
+            st.image(generated_image, caption="Person Wearing Clothing", use_column_width=True)
+      else:
+          st.warning("Please upload both person and clothing images.")
+
+
+# Footer with Font Awesome icons
+footer_html = """
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
+<div style="text-align: center; margin-right: 10%;">
+    <p>
+        &copy; 2024, Trigent Software Inc. All rights reserved. |
+        <a href="https://www.linkedin.com/company/trigent-software" target="_blank" aria-label="LinkedIn"><i class="fab fa-linkedin"></i></a> |
+        <a href="https://www.twitter.com/trigent-software" target="_blank" aria-label="Twitter"><i class="fab fa-twitter"></i></a> |
+        <a href="https://www.youtube.com/trigent-software" target="_blank" aria-label="YouTube"><i class="fab fa-youtube"></i></a>
+    </p>
+</div>
+"""
+# Custom CSS to make the footer sticky
+footer_css = """
+<style>
+.footer {
+    position: fixed;
+    z-index: 1000;
+    left: 0;
+    bottom: 0;
+    width: 100%;
+    background-color: white;
+    color: black;
+    text-align: center;
+}
+[data-testid="stSidebarNavItems"] {
+    max-height: 100%!important;
+}
+</style>
+"""
+# Combining the HTML and CSS
+footer = f"{footer_css}<div class='footer'>{footer_html}</div>"
+# Rendering the footer
+st.markdown(footer, unsafe_allow_html=True)
