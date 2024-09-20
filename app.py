5092d1a9fc475967828ace194268eacf83a373f8
hapijul-trigent <hapijul_h@trigent.com>
2024-09-20 08:54:16 +0530

    update minimal vton pipeline

--- app.py
+++ app.py
@@ -3,6 +3,10 @@ from PIL import Image
 from src.tryon import InpaintingPipeline, load_and_process_image
 from src.body_segmentation import segment_body
 import torch
+from diffusers import AutoPipelineForInpainting, AutoencoderKL
+from diffusers.utils import load_image
+
+
 # Set main panel
 # favicon = Image.open("static/images/Trigent_Logo.png")
 st.set_page_config(
@@ -11,6 +15,8 @@ st.set_page_config(
     layout="wide",
     initial_sidebar_state="collapsed",
 )
+
+
 def get_or_create_session_state_variable(key, default_value=None):
     """
     Retrieves the value of a variable from Streamlit's session state.
@@ -34,7 +40,7 @@ get_or_create_session_state_variable(key='TryOnApp', default_value=False)
 # app = loadTryOnApp()
 # st.session_state['TryOnApp'] = True
 @st.cache_resource()
-def virtual_try_on(img, clothing, prompt, negative_prompt, ip_scale=1.0, strength=0.99, guidance_scale=7.5, steps=100):
+def virtual_try_on_pipeline(ip_scale=1.0):
     vae = AutoencoderKL.from_pretrained("madebyollin/sdxl-vae-fp16-fix", torch_dtype=torch.float16)
     pipeline = AutoPipelineForInpainting.from_pretrained("diffusers/stable-diffusion-xl-1.0-inpainting-0.1",
         vae=vae,
@@ -43,7 +49,11 @@ def virtual_try_on(img, clothing, prompt, negative_prompt, ip_scale=1.0, strengt
         use_safetensors=True
     ).to("cuda")
     pipeline.load_ip_adapter("h94/IP-Adapter", subfolder="sdxl_models", weight_name="ip-adapter_sdxl.bin", low_cpu_mem_usage=True)
-    
+    pipeline.set_ip_adapter_scale(ip_scale)
+    return pipeline
+
+
+def virtual_try_on(pipeline, img, clothing, prompt, negative_prompt, ip_scale=1.0, strength=0.99, guidance_scale=7.5, steps=100):
     _, mask_img = segment_body(img, face=False)
     pipeline.set_ip_adapter_scale(ip_scale)
     images = pipeline(
@@ -59,6 +69,8 @@ def virtual_try_on(img, clothing, prompt, negative_prompt, ip_scale=1.0, strengt
     return images[0]
 
 
+
+vton_pipeline = virtual_try_on_pipeline()
 
 st.title("Virtual Clothing Try-On")
 col1, col2, col3 = st.columns(3)
@@ -86,11 +98,15 @@ settings = {
 if st.button("Generate"):
     print("Ready to generate...")
     if person_image_file and cloth_image_file:
-        person_img = load_and_process_image('person.jpg')
-        cloth_img = load_and_process_image('cloth.jpg')
-        body_mask_image = segment_body(person_img, include_face=True)
-        final_image = app.generate_image(person_img, cloth_img, body_mask_image=body_mask_image, settings=settings)
+        person_img = load_image('person.jpg').convert("RGB")
+        cloth_img = load_image('cloth.jpg').convert("RGB")
+        # final_image = app.generate_image(person_img, cloth_img, body_mask_image=body_mask_image, settings=settings)
+        generated_image = virtual_try_on(
+            pipeline, img=person_img, clothing=cloth_img, 
+            prompt=settings['prompt'], negative_prompt=settings['negative_prompt'], 
+            ip_scale=1.0, strength=0.99, guidance_scale=7.5, steps=100
+        )
         with col3:
-          st.image(final_image, caption="Person Wearing Clothing", use_column_width=True)
+          st.image(generated_image, caption="Person Wearing Clothing", use_column_width=True)
     else:
         st.warning("Please upload both person and clothing images.")
